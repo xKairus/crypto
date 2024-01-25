ad1bc6f68e8e56b4078c3458a8e7b1697ee49d15
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-25 14:32:48 +0500

    dev: add initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { Layout, Flex } from "antd";
-const { Header, Footer, Sider, Content } = Layout;
+import { Layout } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
 const headerStyle = {
   textAlign: "center",
   color: "#fff",
-  height: 64,
+  height: 60,
   paddingInline: 48,
   lineHeight: "64px",
   backgroundColor: "#4096ff",
 };
 const contentStyle = {
   textAlign: "center",
-  minHeight: 120,
+  minHeight: "calc(100vh-60px)",
   lineHeight: "120px",
   color: "#fff",
-  backgroundColor: "#0958d9",
+  backgroundColor: "#001529",
 };
 const siderStyle = {
   textAlign: "center",
@@ -22,27 +24,16 @@ const siderStyle = {
   color: "#fff",
   backgroundColor: "#1677ff",
 };
-const footerStyle = {
-  textAlign: "center",
-  color: "#fff",
-  backgroundColor: "#4096ff",
-};
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: "hidden",
-  width: "calc(50% - 8px)",
-  maxWidth: "calc(50% - 8px)",
-};
+
 const App = () => (
-  <Layout style={layoutStyle}>
-    <Header style={headerStyle}>Header</Header>
+  <Layout>
+    <Layout.Header style={headerStyle}>Header</Layout.Header>
     <Layout>
-      <Sider width="25%" style={siderStyle}>
+      <Layout.Sider width="25%" style={siderStyle}>
         Sider
-      </Sider>
-      <Content style={contentStyle}>Content</Content>
+      </Layout.Sider>
+      <Layout.Content style={contentStyle}>Content</Layout.Content>
     </Layout>
-    <Footer style={footerStyle}>Footer</Footer>
   </Layout>
 );
 export default App;
